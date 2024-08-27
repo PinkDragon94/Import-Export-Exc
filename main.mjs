ec15d88dfbeee197384cdeb61f9b3d4ccd256004
Pinkdragon <cherandwhite@gmail.com>
2024-08-27 13:52:21 -0400

    load files

---
+++ main.mjs
@@ -0,0 +1,11 @@
+// main.js
+
+import { add, subtract, multiply, divide } from './utils.mjs';
+
+let num1 = 6;
+let num2 = 15;
+
+console.log(`The sum of ${num1} and ${num2} is: ${add(num1, num2)}`);
+console.log(`The difference of ${num1} and ${num2} is: ${subtract(num1, num2)}`);
+console.log(`The product of ${num1} and ${num2} is: ${multiply(num1, num2)}`);
+console.log(`The quotient of ${num1} and ${num2} is: ${divide(num1, num2)}`);
